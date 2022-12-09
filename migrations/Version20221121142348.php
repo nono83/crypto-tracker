2603ad28501588c37158fedea9f66a71b7266fce
nono83 <arnaud.gianati@club-internet.fr>
2022-12-09 18:38:04 +0100

    1e commit

---
+++ migrations/Version20221121142348.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221121142348 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE crypto (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, symbol VARCHAR(50) NOT NULL, api_id INT NOT NULL, logo VARCHAR(255) NOT NULL, quantity INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE daily_result (id INT AUTO_INCREMENT NOT NULL, date DATETIME NOT NULL, value INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE investment (id INT AUTO_INCREMENT NOT NULL, crypto_id INT NOT NULL, date DATE NOT NULL, quote NUMERIC(8, 2) NOT NULL, total NUMERIC(10, 2) NOT NULL, INDEX IDX_43CA0AD6E9571A63 (crypto_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE investment ADD CONSTRAINT FK_43CA0AD6E9571A63 FOREIGN KEY (crypto_id) REFERENCES crypto (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE investment DROP FOREIGN KEY FK_43CA0AD6E9571A63');
+        $this->addSql('DROP TABLE crypto');
+        $this->addSql('DROP TABLE daily_result');
+        $this->addSql('DROP TABLE investment');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
---
+++ migrations/Version20221129144616.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221129144616 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE crypto ADD evolution VARCHAR(10) NOT NULL');
+        $this->addSql('ALTER TABLE investment CHANGE total total NUMERIC(8, 2) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE crypto DROP evolution');
+        $this->addSql('ALTER TABLE investment CHANGE total total NUMERIC(9, 2) NOT NULL');
+    }
+}
